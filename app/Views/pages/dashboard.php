4c3d6a684e0e146f53766e51d2b367173ee621ea
nelebaebae <anel.rhx@gmail.com>
2023-11-21 15:34:03 +0700

    Update Landing Page

--- app/Views/pages/dashboard.php
+++ app/Views/pages/dashboard.php
@@ -22,63 +22,61 @@
     <div id="about_ctas_area">
         <div class="about_area_inner_wrapper">
             <div id="about_ctas">
-                <div id="about_cta_community" class="about_cta">
-                    <div class="cta_content">
-                        <h2 class="cta_title">
-                            Join the Community
-                        </h2>
-                        <div class="cta_text">
-                            Meet new people, join groups, form clans, chat in-game and more! With over 100 million
-                            potential friends (or enemies), the fun never stops.
-                        </div>
-                        <div class="cta_btn">
-                            <a href="https://steamcommunity.com/">Visit the Community</a>
-                        </div>
-                    </div>
+                
+                <div class="about_cta">
                     <div class="cta_hero">
-                        <img id="hero_community" class="hero_complete"
-                            src="https://cdn.akamai.steamstatic.com/store/about/cta_hero_community.png">
-                    </div>
-                </div>
-                <div id="about_cta_hardware" class="about_cta">
-                    <div class="cta_hero">
-                        <img id="hero_hardware" class="hero_complete"
-                            src="https://cdn.akamai.steamstatic.com/store/about/cta_hero_hardware.png">
+                        <img class="hero_complete" src="https://cdn.akamai.steamstatic.com/store/about/cta_hero_steamworks_pt1.png">
                     </div>
                     <div class="cta_content">
                         <h2 class="cta_title">
-                            Experience Haze Hardware
+                            Explore and Purchase Games
                         </h2>
                         <div class="cta_text">
-                            We created the Haze Deck and the Valve Index headset to make gaming on the PC even better.
+                            Discover a vast collection of games from various genres. Browse through a curated selection
+                            of AAA titles and indie gems. Purchase and own your favorite games to build your gaming library.
                         </div>
                         <div class="cta_btn">
-                            <a href="https://store.steampowered.com/search/?category1=993">Experience Haze Hardware</a>
+                            <a href="#">Explore Games</a>
                         </div>
                     </div>
                 </div>
-                <div id="about_cta_steamworks" class="about_cta">
+
+                <div class="about_cta">
                     <div class="cta_content">
                         <h2 class="cta_title">
-                            Release your Game
+                            Exclusive Deals
                         </h2>
                         <div class="cta_text">
-                            Haze works is the set of tools and services that help game developers and publishers get the
-                            most out of distributing games on Haze.
+                            Unlock special discounts and exclusive offers on a wide range of games.
                         </div>
                         <div class="cta_btn">
-                            <a href="https://partner.steamgames.com/">Learn about Haze works</a>
+                            <a href="#">Learn More</a>
                         </div>
                     </div>
                     <div class="cta_hero">
-                        <img id="hero_steamworks" class="hero_complete"
-                            src="https://cdn.akamai.steamstatic.com/store/about/cta_hero_steamworks.png">
+                        <img class="hero_complete" src="https://cdn.akamai.steamstatic.com/store/about/cta_hero_hardware_pt2.png?v=2">
                     </div>
                 </div>
+
+                <div class="about_cta">
+                    <div class="cta_hero">
+                        <img class="hero_complete" src="https://cdn.akamai.steamstatic.com/store/about/cta_hero_hardware_pt1.png?v=2">
+                    </div>
+                    <div class="cta_content">
+                        <h2 class="cta_title">
+                            Automatic Updates
+                        </h2>
+                        <div class="cta_text">
+                            Enjoy hassle-free gaming with automatic updates for all your purchased games.
+                        </div>
+                        <div class="cta_btn">
+                            <a href="#">Learn More</a>
+                        </div>
+                    </div>
+                </div>  
             </div>
         </div>
     </div>
-
 
     <div id="about_steam_features_area">
         <div class="about_area_inner_wrapper">
@@ -88,21 +86,6 @@
                 <div id="about_steam_feature_subtitle">
                     We are constantly working to bring new updates and features to Haze, such as: </div>
                 <div id="about_steam_features_grid" class="features_grid">
-
-                    <div id="about_feature_steamchat" class="feature">
-                        <div class="about_feature_icon">
-                            <img src="https://cdn.akamai.steamstatic.com/store/about/icon-steamchat.svg">
-                        </div>
-                        <div class="about_feature_content">
-                            <h3 class="feature_title">
-                                Haze Chat </h3>
-                            <div class="feature_text">
-                                Talk with friends or groups via text or voice without leaving Haze. Videos, Tweets,
-                                GIFs and more are supported; use wisely. </div>
-                            <div class="feature_btn">
-                                Learn More </div>
-                        </div>
-                    </div>
 
                     <div id="about_feature_gamehubs" class="feature">
                         <div class="about_feature_icon">
@@ -114,39 +97,6 @@
                             <div class="feature_text">
                                 Everything about your game, all in one place. Join discussions, upload content, and
                                 be the first to know about new updates. </div>
-                            <div class="feature_btn">
-                                Learn More </div>
-                        </div>
-                    </div>
-
-                    <div id="about_feature_steambroadcasts" class="feature">
-                        <div class="about_feature_icon">
-                            <img src="https://cdn.akamai.steamstatic.com/store/about/icon-broadcasts.svg">
-                        </div>
-                        <div class="about_feature_content">
-                            <h3 class="feature_title">
-                                Steam Broadcast </h3>
-                            <div class="feature_text">
-                                Stream your gameplay live with the click of a button, and share your game with
-                                friends or the rest of the community. </div>
-                            <div class="feature_btn">
-                                Learn More </div>
-                        </div>
-                    </div>
-
-                    <div id="about_feature_steamworkshop" class="feature">
-
-                        <div class="about_feature_icon">
-                            <img src="https://cdn.akamai.steamstatic.com/store/about/icon-steamworkshop.svg">
-                        </div>
-                        <div class="about_feature_content">
-                            <h3 class="feature_title">
-                                Haze Workshop </h3>
-                            <div class="feature_text">
-                                Create, discover, and download player-created mods and cosmetics for nearly 1,000
-                                supported games. </div>
-                            <div class="feature_btn">
-                                Learn More </div>
                         </div>
                     </div>
 
@@ -160,8 +110,6 @@
                             <div class="feature_text">
                                 Access Haze anywhere from your iOS or Android device with the Haze mobile app.
                             </div>
-                            <div class="feature_btn">
-                                Learn More </div>
                         </div>
                     </div>
 
@@ -171,12 +119,10 @@
                         </div>
                         <div class="about_feature_content">
                             <h3 class="feature_title">
-                                Early Access to Games </h3>
+                                Early Access</h3>
                             <div class="feature_text">
                                 Discover, play, and get involved with games as they evolve. Be the first to see
                                 what's coming and become part of the process. </div>
-                            <div class="feature_btn">
-                                Learn More </div>
                         </div>
                     </div>
 
@@ -199,7 +145,7 @@
                         </div>
                         <div class="about_feature_content">
                             <h3 class="feature_title">
-                                Purchases Made Easy </h3>
+                                Easy Purchase </h3>
                             <div class="feature_text">
                                 Our storefront supports 100+ payment methods across over 35 currencies, giving you the
                                 flexibility to pay how you want. </div>
