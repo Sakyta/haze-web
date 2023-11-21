837745fd79e22fc8a1e923587fb03659c5ea8f55
rumirifai <rumirifai96@gmail.com>
2023-11-21 17:43:58 +0700

    Homepage Push

--- app/Views/pages/homepage.php
+++ app/Views/pages/homepage.php
@@ -5,79 +5,113 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Homepage</title>
-    <style>
-        body {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            flex-direction: column;
-            min-height: 100vh;
-            background: #191a19;
-        }
-
-        h1 {
-            background-color: #313331;
-            padding: 7px;
-            border: 1px solid black;
-            border-radius: 10px;
-        }
-
-        .custom-carousel {
-            display: flex;
-            overflow-x: visible;
-            width: 100%;
-
-        }
-
-        .item {
-            flex: 0 0 auto;
-            width: 400px;
-            height: 400px;
-            margin-right: 10px;
-            background-size: cover;
-            background-position: center;
-            border-radius: 10px;
-            cursor: pointer;
-        }
-
-        .item.active {
-            border: 2px solid #3498db;
-        }
-    </style>
+    <link rel="icon" href="../img/Haze.png">
+    <link rel="stylesheet" href="../css/homepage.css">
 </head>
 
 <body>
-    <?php include 'template/header.php';?>
-    
-    <h1>Discover A Game</h1>
-    <div class="custom-carousel">
-        <div class="item" style="
-          background-image: url(https://ucarecdn.com/75d7700d-c102-40ff-9ba1-f0641444c616/dota2.jpg);
-        "></div>
-        <div class="item" style="
-          background-image: url(https://ucarecdn.com/2a5f69bc-befa-49f4-acc6-ab1dcae514c7/winter3.jpg);
-        "></div>
-        <div class="item" style="
-          background-image: url(https://ucarecdn.com/ec1918b1-2674-452c-bf61-8f73d8cc40c6/rdr2.jpg);
-        "></div>
-        <div class="item" style="
-          background-image: url(https://rare-gallery.com/thumbnail/1328261-Yuito-SumeragiScarlet-Nexus-HD-Wallpaper.png);
-        "></div>
+    <?php include 'template/header.php'; ?>
+
+    <div class="slider">
+        <h4>News</h4>
+        <input type="radio" name="choose" id="" class="choose_1">
+        <input type="radio" name="choose" id="" class="choose_2">
+        <div class="slides">
+            <a href="" class="slide s1">
+                <img src="../img/game/slide1.png" alt="" width="600" height="300">
+            </a>
+            <a href="" class="slide">
+                <img src="../img/game/slide2.png" alt="" width="600" height="300">
+            </a>
+        </div>
     </div>
 
-    <script>
-        document.addEventListener("DOMContentLoaded", function() {
-            const items = document.querySelectorAll(".item");
-            items.forEach((item) => {
-                item.addEventListener("click", function() {
-                    items.forEach((i) => i.classList.remove("active"));
-                    this.classList.toggle("active");
-                });
-            });
-        });
-        </script>
-    
-    <?php include 'template/footer.php';?>
+
+    <div class="game-section">
+        <h1>Game Of The Year Nominees</h1>
+        <section class="populer">
+            <div class="item">
+                <a href="">
+                    <img src="../img/game/alanwake2.png" alt="" width="300" height="300">
+                    <div class="judul">Alan Wake II</div>
+                </a>
+            </div>
+            <div class="item">
+                <a href="">
+                    <img src="../img/game/baldursgate3.png" alt="" width="300" height="300">
+                    <div class="judul">Baldurs Gate 3</div>
+                </a>
+            </div>
+            <div class="item">
+                <a href="">
+                    <img src="../img/game/spiderman2.png" alt="" width="300" height="300">
+                    <div class="judul">Marvel : Spider-man 2</div>
+                </a>
+            </div>
+            <div class="item">
+                <a href="">
+                    <img src="../img/game/re4.png" alt="" width="300" height="300">
+                    <div class="judul">Resident Evil 4 Remake</div>
+                </a>
+            </div>
+        </section>
+    </div>
+
+    <div class="game-section">
+        <h1>Game List</h1>
+        <section class="populer">
+            <div class="item">
+                <a href="">
+                    <img src="../img/game/genshin.png" alt="" width="300" height="300">
+                    <div class="judul">Genshin Impact</div>
+                </a>
+            </div>
+            <div class="item">
+                <a href="">
+                    <img src="../img/game/mhw.png" alt="" width="300" height="300">
+                    <div class="judul">Monster Hunter: World</div>
+                </a>
+            </div>
+            <div class="item">
+                <a href="">
+                    <img src="../img/game/gowr.png" alt="" width="300" height="300">
+                    <div class="judul">God of War Ragnarök</div>
+                </a>
+            </div>
+            <div class="item">
+                <a href="">
+                    <img src="../img/game/fishing.png" alt="" width="300" height="300">
+                    <div class="judul">Fishing Planet</div>
+                </a>
+            </div>
+            <div class="item">
+                <a href="">
+                    <img src="../img/game/scarlet.png" alt="" width="300" height="300">
+                    <div class="judul">Scarlet Nexus</div>
+                </a>
+            </div>
+            <div class="item">
+                <a href="">
+                    <img src="../img/game/phasmo.png" alt="" width="300" height="300">
+                    <div class="judul">Phasmophobia</div>
+                </a>
+            </div>
+            <div class="item">
+                <a href="">
+                    <img src="../img/game/unbound.png" alt="" width="300" height="300">
+                    <div class="judul">A Space For The Unbound</div>
+                </a>
+            </div>
+            <div class="item">
+                <a href="">
+                    <img src="../img/game/nier.png" alt="" width="300" height="300">
+                    <div class="judul">Nier: Automata</div>
+                </a>
+            </div>
+        </section>
+    </div>
+
+    <?php include 'template/footer.php'; ?>
 </body>
 
 </html>
