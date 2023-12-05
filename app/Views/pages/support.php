2a705471a8a46a44a043c38a3ddb234919c670df
rumirifai <rumirifai96@gmail.com>
2023-12-05 19:46:36 +0700

    Support Page solve no game

--- app/Views/pages/support.php
+++ app/Views/pages/support.php
@@ -14,24 +14,29 @@
         <div class="help">
             What do you need help with, <?= $player->nickname ?>?
         </div>
-        <div class="recent-product">
-            Recent products
-        </div>
-        <?php $counter = 0 ?>
-        <?php foreach ($games as $row) : ?>
-            <?php $counter++ ?>
-            <div class="additional-box">
-                <img src="data:image/png;base64, <?= $row['game_pic'] ?>" alt="" width="50" height="50">
-                <p><?= $row['game_name'] ?></p>
-                <div class="triangle"></div>
+        <?php if (!empty($games)) : ?>
+            <div class="recent-product">
+                Recent products
             </div>
-            <?php 
-            if ($counter == 3)
-            {
-                break;
-            }
-            ?>
-        <?php endforeach; ?>
+            <?php $counter = 0 ?>
+            <?php foreach ($games as $row) : ?>
+                <?php $counter++ ?>
+                <div class="additional-box">
+                    <img src="data:image/png;base64, <?= $row['game_pic'] ?>" alt="" width="50" height="50">
+                    <p><?= $row['game_name'] ?></p>
+                    <div class="triangle"></div>
+                </div>
+                <?php
+                if ($counter == 3) {
+                    break;
+                }
+                ?>
+            <?php endforeach; ?>
+        <?php else : ?>
+            <div class="no-game">
+                You have no game
+            </div>
+        <?php endif; ?>
         <div class="persegi"> </div>
         <a class="additional-box" href="/myAccount">
             <p>My Account</p>
